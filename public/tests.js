32557c7a81cd7435b20920a2c1f3ba140142fa49
Cristian Luis <alu0100693386@ull.edu.es>
2014-03-20 19:29:11 +0000

    Testes

---
+++ public/tests.js
@@ -0,0 +1,15 @@
+var assert = chai.assert;
+
+suite('Analizador sintactico ', function() {
+    test('Factor', function() {
+      var result, source;
+      source = "a = 8 + 3 * 9";
+      try {
+	result = JSON.stringify(parse(source), null, 2);
+      } catch (_error) {
+	result = _error;
+	result = "<div class=\"error\">" + result + "</div>";
+      }
+      assert.deepEqual(result, );
+    });
+}
