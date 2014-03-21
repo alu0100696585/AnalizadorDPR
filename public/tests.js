cd1003e6b76eb9e32f9d9e4504e6bec770dd61e1
Cristian Luis <alu0100693386@ull.edu.es>
2014-03-21 09:03:45 +0000

    Pruebas

--- public/tests.js
+++ public/tests.js
@@ -5,27 +5,6 @@ suite('Analizador sintactico ', function() {
       var r = "A = 35.";
       var resultado;
       resultado = main(r);
-      assert.deepEqual(resultado, "[\n  {\n    "type": "=",\n    "left": {\n      "type": "ID",\n      "value": "A"\n    },\n    "right": {\n      "type": "NUM",\n      "value": 35\n    }\n  }\n]" );
-    });
-    
-    test('Factor', function() {
-      var r = "A = 35.";
-      var resultado;
-      resultado = main(r);
-      assert.deepEqual(resultado, "[\n  {\n    "type": "=",\n    "left": {\n      "type": "ID",\n      "value": "A"\n    },\n    "right": {\n      "type": "NUM",\n      "value": 35\n    }\n  }\n]" );
-    });
-    
-    test('Factor', function() {
-      var r = "A = 35.";
-      var resultado;
-      resultado = main(r);
-      assert.deepEqual(resultado, "[\n  {\n    "type": "=",\n    "left": {\n      "type": "ID",\n      "value": "A"\n    },\n    "right": {\n      "type": "NUM",\n      "value": 35\n    }\n  }\n]" );
-    });
-    
-    test('Factor', function() {
-      var r = "A = 35.";
-      var resultado;
-      resultado = main(r);
-      assert.deepEqual(resultado, "[\n  {\n    "type": "=",\n    "left": {\n      "type": "ID",\n      "value": "A"\n    },\n    "right": {\n      "type": "NUM",\n      "value": 35\n    }\n  }\n]" );
+      assert.deepEqual(r, "" );
     });
 });
