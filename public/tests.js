9c6393a7943e8db20b31e6665fcad022490a0486
Cristian Luis <alu0100693386@ull.edu.es>
2014-03-21 09:08:14 +0000

    Pruebas

--- public/tests.js
+++ public/tests.js
@@ -5,6 +5,6 @@ suite('Analizador sintactico ', function() {
       var r = "A = 35.";
       var resultado;
       resultado = main(r);
-      assert.equal(resultado, "" );
+      assert.equal(resultado, "[\n  {\n    \"type\": \"=\",\n    \"left\": {\n      \"type\": \"ID\",\n      \"value\": \"A\"\n    },\n    \"right\": {\n      \"type\": \"NUM\",\n      \"value\": 35\n    }\n  }\n]" );
     });
 });
